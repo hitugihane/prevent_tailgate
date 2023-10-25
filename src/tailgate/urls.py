aac85e87c4a2b9b8c3bd76d5d9de90e775ec86d2
hitugihane <hituzihane01@gmail.com>
2023-10-25 11:08:57 +0900

    hogehoege

--- src/tailgate/urls.py
+++ src/tailgate/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
   path ('login/',views.CustmLoginView.as_view(),name='login'),
   path ('home',views.HomeView.as_view(),name='home'),
-   path('webcam/', views.WebcamView.as_view(), name='webcam'),
+  #path('webcam/', views.WebcamView.as_view(), name='webcam'),
+  #path('detect/', views.TailgateDetectionView.as_view(), name='tailgate_detection'),
+  path('detect/', views.FaceDetectionView.as_view(), name='webcam'),
 ]
